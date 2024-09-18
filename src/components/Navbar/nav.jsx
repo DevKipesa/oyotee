c5cd93c8cd056c104623dbcc69ccad859526e5a8
DevKipesa <kymdommie@gmail.com>
2024-09-18 20:56:38 +0300

    making it responsive

--- src/components/Navbar/nav.jsx
+++ src/components/Navbar/nav.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FiShoppingCart, FiHeart, FiUser } from 'react-icons/fi'; // Icons from react-icons
+import { FiShoppingCart, FiHeart, FiUser, FiMenu, FiX } from 'react-icons/fi'; 
 import './nav.css';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="logo">
         <Link to="/">Oyotee</Link>
       </div>
 
-      <ul className="nav-links">
+      <div className="menu-icon" onClick={toggleMenu}>
+        {isMenuOpen ? <FiX /> : <FiMenu />}
+      </div>
+
+      <ul className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
         <li><Link to="/shop">Shop</Link></li>
         <li><Link to="/plantcare">Plantcare</Link></li>
         <li><Link to="/workshops">Workshops</Link></li>
